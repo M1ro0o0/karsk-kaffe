65c405295cc6dea120a17eeddf8ad2950b96e507
M1ro0o0o <miroslavandrejcak2004@gmail.com>
2026-04-10 00:12:58 +0200

    PAI FOR DISCOUNT

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
-app.use(express.json());
+const app = express();
 
 /*******************
       Database
