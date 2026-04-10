143dfe4bc9b534609cf22a675831268fdbbf6aff
M1ro0o0o <miroslavandrejcak2004@gmail.com>
2026-04-10 17:24:41 +0200

    now it shod work, pease

--- Backend/server.js
+++ Backend/server.js
@@ -8,8 +8,6 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
-const cors = require("cors");
-
 app.use(cors());
 app.options('*', cors());
 app.use(express.json());
