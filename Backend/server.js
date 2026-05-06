86db38cdef9b5bb3e2626e4faf94fbedb4ddf49f
M1ro0o0o <miroslavandrejcak2004@gmail.com>
2026-05-06 11:50:48 +0200

    Zoho refresh token func added

--- Backend/server.js
+++ Backend/server.js
@@ -78,6 +78,24 @@ function formatProductCard(product, lang = "en") {
   };
 }
 
+async function getZohoAccessToken() {
+  const response = await fetch("https://accounts.zoho.eu/oauth/v2/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: new URLSearchParams({
+      grant_type: "refresh_token",
+      client_id: process.env.ZOHO_CLIENT_ID,
+      client_secret: process.env.ZOHO_CLIENT_SECRET,
+      refresh_token: process.env.ZOHO_REFRESH_TOKEN
+    })
+  });
+
+  const data = await response.json();
+  return data.access_token;
+}
+
 /*--------------
       APIs
 --------------*/
