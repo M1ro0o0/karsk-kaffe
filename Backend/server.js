cc72b1e25194c557da0639449f50419338edadba
M1ro0o0o <miroslavandrejcak2004@gmail.com>
2026-05-14 17:34:47 +0200

    test

--- Backend/server.js
+++ Backend/server.js
@@ -25,8 +25,8 @@ const productRoutes =
 const discountRoutes =
   require("./routes/discounts");
 
-const shippingRoutes =
-  require("./routes/shipping");
+/*const shippingRoutes =
+  require("./routes/shipping");*/
 
 /*--------------------
         APP
